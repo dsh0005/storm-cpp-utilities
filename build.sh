cddb7e159e7f1ad47d629f286806613807331935
Douglas Storm Hill <stormhill117@gmail.com>
2023-04-01 11:57:25 -0500

    Add a basic microbenchmark.
    Add a basic microbenchmark that measures wall time, and CPU time. Wall
    time does measure speed, but I'm more focused on measuring efficiency,
    which is better done by CPU time.
    
    Unfortunately, the builds are getting annoyingly long, so it's about
    time to use a makefile or something.

--- build.sh
+++ build.sh
@@ -11,25 +11,41 @@ RELRO_FLAGS="-Wl,-z,relro,-z,now"
 INCLUDES="-I./containers"
 
 # build tests
+
+printf 'building normal tests: '
 g++ $BASIC_CXX_FLAGS $BASIC_LINK_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
   -Og -ggdb3 $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
   test/mpmc_queue_tests.cpp \
   -o build/mpmc_queue_tests
+printf 'done!\n'
 
+printf 'building ASAN tests: '
 g++ $BASIC_CXX_FLAGS $BASIC_LINK_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
   -Og -ggdb3 $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
   -fsanitize=address -fno-omit-frame-pointer \
   test/mpmc_queue_tests.cpp \
   -o build/mpmc_queue_tests_asan
+printf 'done!\n'
 
+printf 'building TSAN tests: '
 g++ $BASIC_CXX_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
   -Og -ggdb3 $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
   -fsanitize=thread \
   test/mpmc_queue_tests.cpp \
   -o build/mpmc_queue_tests_tsan
+printf 'done!\n'
 
+printf 'building UBSAN tests: '
 g++ $BASIC_CXX_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
   -Og -ggdb3 $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
   -fsanitize=undefined \
   test/mpmc_queue_tests.cpp \
   -o build/mpmc_queue_tests_ubsan
+printf 'done!\n'
+
+printf 'building microbenchmark: '
+g++ $BASIC_CXX_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
+  -O2 -g $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
+  test/mpmc_bench.cpp \
+  -o build/mpmc_bench
+printf 'done!\n'
---
+++ test/mpmc_bench.cpp
@@ -0,0 +1,191 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/* mpmc_bench: Simple microbenchmarks for the mpmc_queue.
+ * Copyright 2023, Douglas Storm Hill
+ *
+ * Provided under AGPLv3 (only), see LICENSE file
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+#include <iomanip>
+#include <thread>
+#include <future>
+#include <latch>
+#include <vector>
+#include <chrono>
+#include <map>
+
+#include <cstddef>
+#include <ctime>
+
+#include "mpmc_queue.hpp"
+using namespace storm;
+
+using std::cout;
+
+// put n items into q
+// value: the value to put copies of into q
+template<typename T>
+static void normal_producer(
+		const std::shared_ptr<mpmc_queue<T>> q,
+		const int n,
+		const T value,
+		std::latch &start_signal,
+		std::latch &stop_signal
+		){
+	start_signal.arrive_and_wait();
+
+	for(int i = 0; i < n; i++){
+		q->push(value);
+	}
+
+	stop_signal.arrive_and_wait();
+}
+
+// pop n items from q, using pop_wait
+template<typename T>
+static void normal_consumer(
+		const std::shared_ptr<mpmc_queue<T>> q,
+		const int n,
+		std::latch &start_signal,
+		std::latch &stop_signal
+		){
+	start_signal.arrive_and_wait();
+
+	for(int i = 0; i < n; i++){
+		q->pop_wait();
+	}
+
+	stop_signal.arrive_and_wait();
+}
+
+// How much time was taken by a benchmark.
+struct concurrency_test_time {
+	std::chrono::steady_clock::duration wall_time;
+	std::clock_t cpu_time;
+};
+
+// test with producer(s) and consumer(s) on different threads
+static concurrency_test_time test_with_concurrency(const int producers, const int consumers){
+	// Here's the queue we'll be testing.
+	auto q = std::make_shared<mpmc_queue<float>>();
+
+	static constexpr int num_items = 10'000'000;
+
+	// This is the wall clock start time.
+	std::chrono::time_point<std::chrono::steady_clock> wall_start;
+	// And here's the process CPU start time.
+	std::clock_t cpu_start;
+
+	// This is to _try_ to reduce timing overhead from startup.
+	// +1 for us so we can time it.
+	std::latch start(producers+consumers+1);
+	std::latch stop(producers+consumers+1);
+
+	// Here's where we keep the futures for the producer and consumer tasks.
+	std::vector<std::future<void>> producer_futs;
+	std::vector<std::future<void>> consumer_futs;
+
+	// Here's a naive estimate of how many items per worker to run.
+	const int items_per_producer = num_items / producers;
+	const int items_per_consumer = num_items / consumers;
+
+	// And here's where we keep track of the number of items left.
+	int producer_items_left = num_items;
+	int consumer_items_left = num_items;
+
+	// We distribute extra onto the last worker to avoid any questions about
+	// rounding in the division op.
+	for(int i = 0; i < producers-1; i++){
+		producer_futs.push_back(
+			std::async(std::launch::async,
+				normal_producer<float>, q, items_per_producer, 1.0f,
+				std::ref(start), std::ref(stop)));
+
+		producer_items_left -= items_per_producer;
+	}
+	for(int i = 0; i < consumers-1; i++){
+		consumer_futs.push_back(
+			std::async(std::launch::async,
+				normal_consumer<float>, q, items_per_consumer,
+				std::ref(start), std::ref(stop)));
+
+		consumer_items_left -= items_per_consumer;
+	}
+
+	// Now put the remaining work on the last workers.
+	producer_futs.push_back(
+		std::async(std::launch::async,
+			normal_producer<float>, q, producer_items_left, 1.0f,
+			std::ref(start), std::ref(stop)));
+	producer_items_left = 0;
+	consumer_futs.push_back(
+		std::async(std::launch::async,
+			normal_consumer<float>, q, consumer_items_left,
+			std::ref(start), std::ref(stop)));
+	consumer_items_left = 0;
+
+	// Now that everything's set up, start the timers and the test.
+	wall_start = std::chrono::steady_clock::now();
+	cpu_start = std::clock();
+	start.arrive_and_wait();
+
+	// Stop the test, stop the timers and return the results.
+	stop.arrive_and_wait();
+	const auto wall_stop = std::chrono::steady_clock::now();
+	auto cpu_stop = std::clock();
+
+	// We could loop over the vectors and wait, but why do that when
+	// the destructors do the job for us?
+
+	return concurrency_test_time{
+		wall_stop - wall_start,
+		cpu_stop - cpu_start,
+	};
+}
+
+using test_results_map = std::map<std::pair<int, int>, concurrency_test_time>;
+
+static void print_results(const test_results_map &map){
+	using std::setw;
+	using std::right;
+	// Might as well do it by value, since it's like 16 bytes.
+	for(const auto [concurrency, times] : map){
+		cout << concurrency.first << " Producer ";
+		cout << concurrency.second << " Consumer, ";
+		cout << "wall: " << right << setw(12) << times.wall_time;
+		cout << " cpu: " << right << setw(9) << times.cpu_time << '\n';
+	}
+}
+
+int main(int /* argc */, char ** /* argv */){
+	// Here's where we store our results.
+	test_results_map test_results;
+
+	cout << "Running basic single-producer single-consumer tests.\n";
+	test_results.insert_or_assign(std::make_pair(1, 1), test_with_concurrency(1, 1));
+
+	cout << "Running MPMC tests with small amounts of concurrency.\n";
+	cout << "1p2c: " << std::flush;
+	test_results.insert_or_assign(std::make_pair(1, 2), test_with_concurrency(1, 2));
+	cout << "done\n";
+	cout << "2p1c: " << std::flush;
+	test_results.insert_or_assign(std::make_pair(2, 1), test_with_concurrency(2, 1));
+	cout << "done\n";
+	cout << "2p2c: " << std::flush;
+	test_results.insert_or_assign(std::make_pair(2, 2), test_with_concurrency(2, 2));
+	cout << "done\n";
+
+	print_results(test_results);
+}
