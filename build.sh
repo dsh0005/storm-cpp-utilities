271ae35efd3f132f24415b53a13771c2aef7324d
Douglas Storm Hill <stormhill117@gmail.com>
2023-04-01 10:23:54 -0500

    Add ASAN and TSAN builds.
    Add ASAN and TSAN builds to the build script. ASAN comes up clean, but I
    can't get TSAN to run correctly. Looks like it's time to emerge clang so
    I can try that out.

--- build.sh
+++ build.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 
+BASIC_CXX_FLAGS="-std=c++20 -U_FORTIFY_SOURCE"
+BASIC_LINK_FLAGS=""
+BASIC_WARN_FLAGS="-Wall -Wextra -pedantic"
+
+PIE_FLAGS="-pie -fno-plt"
+FORTIFY_FLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2"
+RELRO_FLAGS="-Wl,-z,relro,-z,now"
+
+INCLUDES="-I./containers"
+
 # build tests
-g++ -std=c++20 -Og -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -Wall -Wextra \
-  -pedantic -pie -fno-plt -Wl,-z,relro,-z,now \
-  -I./containers \
+g++ $BASIC_CXX_FLAGS $BASIC_LINK_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
+  -Og -ggdb3 $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
   test/mpmc_queue_tests.cpp \
   -o build/mpmc_queue_tests
+
+g++ $BASIC_CXX_FLAGS $BASIC_LINK_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
+  -Og -ggdb3 $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
+  -fsanitize=address -fno-omit-frame-pointer \
+  test/mpmc_queue_tests.cpp \
+  -o build/mpmc_queue_tests_asan
+
+g++ $BASIC_CXX_FLAGS $BASIC_WARN_FLAGS $INCLUDES \
+  -Og -ggdb3 $FORTIFY_FLAGS $PIE_FLAGS $RELRO_FLAGS \
+  -fsanitize=thread \
+  test/mpmc_queue_tests.cpp \
+  -o build/mpmc_queue_tests_tsan
